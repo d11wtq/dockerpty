4bbe4ab3a3c9f4d69cb8e63ebe80855bddc29ca9
d11wtq <chris@w3style.co.uk>
2014-06-25 15:34:03 +0000

    Version bump 0.0.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(filename):
 
 setup(
     name='dockerpty',
-    version='0.0.7',
+    version='0.0.8',
     description='Python library to use the pseudo-tty of a docker container',
     long_description=read('README.md'),
     url='https://github.com/d11wtq/dockerpty',
